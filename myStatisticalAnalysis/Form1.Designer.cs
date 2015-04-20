eac58e0f1b90f9a77c668019b6bc0dbb1a74f5c5
unknown <Administrator@DARINDAI-PC.RTEC.NET>
2015-04-20 13:31:55 +0800

    init

---
+++ myStatisticalAnalysis/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace myStatisticalAnalysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sPCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileFToolStripMenuItem,
+            this.sPCToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(539, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileFToolStripMenuItem
+            // 
+            this.fileFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openpToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileFToolStripMenuItem.Name = "fileFToolStripMenuItem";
+            this.fileFToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.fileFToolStripMenuItem.Text = "File(&F)";
+            // 
+            // openpToolStripMenuItem
+            // 
+            this.openpToolStripMenuItem.Name = "openpToolStripMenuItem";
+            this.openpToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openpToolStripMenuItem.Text = "Open(&O)";
+            this.openpToolStripMenuItem.Click += new System.EventHandler(this.openpToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // sPCToolStripMenuItem
+            // 
+            this.sPCToolStripMenuItem.Name = "sPCToolStripMenuItem";
+            this.sPCToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.sPCToolStripMenuItem.Text = "SPC";
+            this.sPCToolStripMenuItem.Click += new System.EventHandler(this.sPCToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(434, 213);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 246);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(434, 101);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 396);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem sPCToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ myStatisticalAnalysis/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.OleDb;
+namespace myStatisticalAnalysis
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public DataSet RawDataSet=new DataSet();
+        public DataTable RawDataTable = new DataTable();
+        public DataSet readExcel(string excelpath)
+        {
+            string strCon = " Provider = Microsoft.Jet.OLEDB.4.0 ; Data Source = "+excelpath +";Extended Properties=Excel 8.0";
+            OleDbConnection myConn = new OleDbConnection(strCon);
+            myConn.Open();
+
+            DataTable dtSheetName = myConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "Table" });
+
+            string[] strTableNames = new string[dtSheetName.Rows.Count];
+            for (int k = 0; k < dtSheetName.Rows.Count; k++)
+            {
+                strTableNames[k] = dtSheetName.Rows[dtSheetName.Rows.Count - k - 1]["TABLE_NAME"].ToString();
+            }
+
+           // string strCom = " SELECT * FROM [Sheet1$] ";
+              RawDataSet.Clear();
+              for (int k = 0; k < strTableNames.Length; k++)
+              {
+                  string strCom = " SELECT * FROM [" + strTableNames[k] + "] ";
+
+                  OleDbDataAdapter myCommand = new OleDbDataAdapter(strCom, myConn);
+                  myCommand.Fill(RawDataSet, "[" + strTableNames[k] + "]");
+              }
+  
+          //  myDataSet = new DataSet();
+           
+           // myCommand.Fill(RawDataSet, "[Sheet1$]");      
+            
+            //  myCommand.Fill(
+           // RawDataSet.t
+            myConn.Close();
+            return RawDataSet;
+        }
+        private void openpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          //  this.openFileDialog1.Filter = "*.xls";
+            this.openFileDialog1.Filter = "(*.xls)|*.xls";
+            if(this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string FileName = this.openFileDialog1.FileName;
+                readExcel(FileName);
+               // RawDataTable = RawDataSet.Tables["[demo1]"];
+                RawDataTable = RawDataSet.Tables["[Sheet1$]"];
+               // dataGridView1.DataMember = "[Sheet1$]";
+                this.dataGridView1.DataSource = RawDataTable;
+                int rowNumber = 1;
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    row.HeaderCell.Value = rowNumber.ToString();
+                    rowNumber++;
+                }
+                for (int i = 0; i < this.dataGridView1.Columns.Count; i++)
+                {
+                    this.dataGridView1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                }
+               //this.dataGridView1.Refresh();
+                // 你的 处理文件路径代码 
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        int i = 10;
+        public Bitmap createChartImage(int Width, int Height)
+        {
+            System.Drawing.Bitmap image1 = new Bitmap(Width, Height);
+            Graphics g = Graphics.FromImage(image1);
+            g.Clear(Color.Black);
+            g.DrawLine(Pens.Gold, 10 + i, 10, 110, 10 + i);
+            Point[] pp ={ new Point(1, 2), new Point(2, 3), new Point(5, 7), new Point(9, 7), new Point(9, 1) };
+
+            g.DrawLines(Pens.Gold, pp);
+            i += 10;
+            Font font = new Font("宋体", 30f); //字是什么样子的？
+
+            Brush brush = Brushes.Red; //用红色涂上我的字吧；
+            string str = "Baidu"; //写什么字？
+
+//Font font =new  Font("宋体",30f); //字是什么样子的？
+
+//Brush brush = Brushes.Red; //用红色涂上我的字吧；
+
+PointF point = new PointF(10f,10f); //从什么地方开始写字捏？
+
+ 
+
+//横着写还是竖着写呢？
+
+System.Drawing.StringFormat sf = new System.Drawing.StringFormat();
+
+//还是竖着写吧
+
+sf.FormatFlags = StringFormatFlags.DirectionVertical;
+
+ 
+
+//开始写咯
+
+g.DrawString(str,font,brush,point,sf);
+
+ 
+g.DrawString("hello", font, brush,20f, 20f);
+            g.Dispose();
+            return image1;
+        }
+        private void sPCToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+            this.pictureBox1.Image = createChartImage(pictureBox1.Width, pictureBox1.Height);
+        }
+    }
+}
